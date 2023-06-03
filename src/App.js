e95f7b284dad9d4819b77c0d12ce3e48fbf91dec
Mustafasohail7 <66203849+Mustafasohail7@users.noreply.github.com>
2023-06-03 11:35:10 +0000

    fixed redundant code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutIcon from "./components/AboutIcon"
 import About from './pages/About'
-import { useState } from "react"
 import { FeedbackProvider } from "./context/FeedbackContext"
 
 function App(){
-
-    const [feedback,setFeedback] = useState(FeedbackData)
 
 
     return (
